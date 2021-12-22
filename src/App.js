8c51aa5bc0a06445d6842e10f00843eb89a15789
Karthik H <32575042+karthikh07@users.noreply.github.com>
2021-12-23 02:02:08 +0530

    6

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and k2 save to reload.
+          Edit <code>src/App.js</code> and k23 save to reload.
         </p>
         <a
           className="App-link"
